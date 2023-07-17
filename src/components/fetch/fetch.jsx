a8e95bee5b565e183b18c32474ba61ffde0cfdc4
FuryxSAMURAI <szaitsev000@gmail.com>
2023-07-17 15:33:43 +0300

    aded trainee UA

--- src/components/fetch/fetch.jsx
+++ src/components/fetch/fetch.jsx
@@ -10,16 +10,16 @@ function FetchData() {
       const jsonData = await response.json();
       setData(jsonData);
     } catch (error) {
-      console.error('Ошибка при получении данных:', error);
+      console.error('Ой, щось пішло не так:', error);
     }
   };
 
   return (
     <div>
-      <button className='btn' onClick={fetchData}>Загрузить данные</button>
+      <button className='btn' onClick={fetchData}>Показати fetch API</button>
       {data && (
         <div className='gotData'>
-          <h4>Полученные данные:</h4>
+          <h4>Данні з API:</h4>
           <pre>{JSON.stringify(data, null, 2)}</pre>
         </div>
       )}
